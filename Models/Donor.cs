bc46548f7f0c213142519290b2775e44a4ea23a1
jcpanganiban <jcpanganiban6@gmail.com>
2024-11-18 10:41:20 +0800

    Added Name, Email, and Contact Number fields in Model

--- Models/Donor.cs
+++ Models/Donor.cs
@@ -7,5 +7,8 @@ namespace donation.Models
 {
     public class Donor
     {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string ContactNumber { get; set; }
     }
 }
