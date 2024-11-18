98d69ebcc4840b5d09422e9b17c632a780530958
jcpanganiban <jcpanganiban6@gmail.com>
2024-11-18 10:20:54 +0800

    Updates: - Added missing NuGet Packages for Donor Form functionality - Added json, service, and interface for Donors

---
+++ Interfaces/IDonorService.cs
@@ -0,0 +1,15 @@
+﻿using donation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace donation.Interfaces
+{
+    public interface IDonorService
+    {
+        Task<Donor> CreateDonor(Donor donor);
+        List<Donor> GetDonors();
+    }
+}
