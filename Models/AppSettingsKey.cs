5698c9e59a436b0a0d4de02050ef0df53bd2ab61
jcpanganiban <jcpanganiban6@gmail.com>
2024-11-19 11:58:44 +0800

    Added version 1.0.0.R0001P

---
+++ Models/AppSettingsKey.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace donation.Models
+{
+    public class AppSettingsKey
+    {
+        public const string Version = "Version";
+    }
+}
